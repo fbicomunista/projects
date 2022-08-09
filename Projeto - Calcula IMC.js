531b7be2fad88d3b81b4919d0610ece379c4acd2
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-09 06:31:55 -0300

    Update Projeto - Calcula IMC.js

--- Projeto - Calcula IMC.js
+++ Projeto - Calcula IMC.js
@@ -11,13 +11,13 @@
   var peso = parseInt(number(prompt("Ola! digite aqui o seu peso: ")))
   var altura = parseInt(number(prompt("Massa! agora digite a sua altura por favor: ")))
    for(let i=0; i>0; ++i){
-    if(peso<43){prompt("Voce mora na Africa? ")}
+    if(CalculaIMC < 18,5){(prompt("Voce mora na Africa? ")}
      if("sim" || "não"){document.write("Sem piadinhas com a Africa ok? digite o seu real peso paliaso." i++)
      } 
-      if(peso<=68){mostra("Voce esta muito magro, precisa urgentemente comer mais, mesmo voce não precisando de chaves para abrir portas, isso não faz bem para a sua saude, recomendo que vá ao medico e leia esse artigo aqui #vai algum link aqui " i++)
+      if(CalculaIMC<=68){mostra("Voce esta muito magro, precisa urgentemente comer mais, mesmo voce não precisando de chaves para abrir portas, isso não faz bem para a sua saude, recomendo que vá ao medico e leia esse artigo aqui #vai algum link aqui " i++)
       }
-       if(peso<=83){mostra("Parabens, voce esta na media! não se preocupe, voce não vai voar com o vento, nem ser confundido com um pneu de posto de Gasolina, siga se cuidando assim, aproveite que voce esta parecendo um deus grego e leia esse artigo sobre saude aqui #vai algum link aqui "      i++)}
-        if(peso>=90){mostra("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou praticar Sumo" i++)
+       if(CalculaIMC<=83){mostra("Parabens, voce esta na media! não se preocupe, voce não vai voar com o vento, nem ser confundido com um pneu de posto de Gasolina, siga se cuidando assim, aproveite que voce esta parecendo um deus grego e leia esse artigo sobre saude aqui #vai algum link aqui "      i++)}
+        if(CalculaIMC>=90){mostra("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou praticar Sumo" i++)
    else{"desculpe, mas não entendi pulufas do que voce disse!" i++)
    }}
 </script>
