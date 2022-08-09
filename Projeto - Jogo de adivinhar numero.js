083ae25a7804009830849c36f9eda3c42c23eeef
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-09 06:17:21 -0300

    Update Projeto - Jogo de adivinhar numero.js

--- Projeto - Jogo de adivinhar numero.js
+++ Projeto - Jogo de adivinhar numero.js
@@ -1,23 +1,25 @@
 
-<meta charset="UTF-8">
+<!DOCTYPE html>
+<html>
+<head lang="pt-br">
+ <meta charset="UTF-8">
+</head>
 <script>
-
-	var numeroPensado = Math.round(Math.random() * 10) ;
-    var tentativas = 1;
-  
-    	while(tentativas <= 3){
-
-		var chute = parseInt(prompt("Digite seu chute: "));
-
-			if(chute == numeroPensado){
-         	 console.log("Você descobriu!, o número pensado era "+numeroPensado);
-			break;
-          } else {
-				console.log("Você não descobriu antes das 3 tentativas!");
+ var numeroPensado = Math.round(Math.random() * 10) ;
+ var tentativas = 1;
+  while(tentativas <= 3){
+   var chute = parseInt(prompt("Digite seu chute: "));
+    if(chute == numeroPensado){
+     console.log("Você descobriu!, o número pensado era "+numeroPensado);
+    break;
+  } else {
+ console.log("Você não descobriu antes das 3 tentativas!");
 		}
-        tentativas++;
+tentativas++;
 	}
 
 console.log("fim");
 
+
 </script>
+</html>
