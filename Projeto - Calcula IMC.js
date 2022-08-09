3104cc9d24da89c21fe8905c536404b3faaef937
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-09 06:35:37 -0300

    Update Projeto - Calcula IMC.js

--- Projeto - Calcula IMC.js
+++ Projeto - Calcula IMC.js
@@ -11,12 +11,12 @@
   var peso = parseInt(number(prompt("Ola! digite aqui o seu peso: ")))
   var altura = parseInt(number(prompt("Massa! agora digite a sua altura por favor: ")))
    for(let i=0; i>0; ++i){
-    if(CalculaIMC < 18,5){(prompt("Voce mora na Africa? ")}
+    if(CalculaIMC<18.5){(prompt("Voce mora na Africa? ")}
      if("sim" || "não"){document.write("Sem piadinhas com a Africa ok? digite o seu real peso paliaso." i++)
      } 
-      if(CalculaIMC<=68){mostraUmaFrase("Voce esta muito magro, precisa urgentemente comer mais, mesmo voce não precisando de chaves para abrir portas, isso não faz bem para a sua saude, recomendo que vá ao medico e leia esse artigo aqui #vai algum link aqui " i++)
+      if(CalculaIMC<25){mostraUmaFrase("Parabens, voce esta na media! não se preocupe, voce não vai voar com o vento, nem ser confundido com um pneu de posto de Gasolina, siga se cuidando assim, aproveite que voce esta parecendo um deus grego e leia esse artigo sobre saude aqui #vai algum link aqui "      i++)
       }
-       if(CalculaIMC<=83){mostraUmaFrase("Parabens, voce esta na media! não se preocupe, voce não vai voar com o vento, nem ser confundido com um pneu de posto de Gasolina, siga se cuidando assim, aproveite que voce esta parecendo um deus grego e leia esse artigo sobre saude aqui #vai algum link aqui "      i++)}
+       if(CalculaIMC<=83){mostraUmaFrase()}
         if(CalculaIMC>=90){mostraUmaFrase("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou praticar Sumo" i++)
    else{"desculpe, mas não entendi pulufas do que voce disse!" i++)
    }}
