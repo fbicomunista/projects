bac34c93e7f2fbd6cffc5c362a7069934a6203d0
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-09 06:51:44 -0300

    Update Projeto - Calcula IMC.js

--- Projeto - Calcula IMC.js
+++ Projeto - Calcula IMC.js
@@ -15,8 +15,8 @@
      if("sim" || "não"){document.write("Sem piadinhas com a Africa ok? digite o seu real peso paliaso." i++)
      } 
       if(CalculaIMC<25){mostraUmaFrase("Parabens seu IMC esta na media! Voce esta parecendo um(a) deus(a) grego(a), não se preocupe, voce não vai voar com o vento nem ser confundido(a) com um pneu de posto de Gasolina, siga se cuidando assim!" i++)}
-       if(CalculaIMC<=30){mostraUmaFrase("Voce esta com sobrepeso, esta parecendo o Majin Boo, precisa fazer mais exercicios e se alimentar melhor." i++)}
-        if(CalculaIMC>=90){mostraUmaFrase("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou praticar Sumo." i++)}
+       if(CalculaIMC<30){mostraUmaFrase("Voce esta com sobrepeso, esta parecendo o Majin Boo, precisa fazer mais exercicios e se alimentar melhor." i++)}
+        if(CalculaIMC>30){mostraUmaFrase("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou começar a praticar Sumo." i++)}
    else{mostraUmaFrase("desculpe, mas não entendi pulufas do que voce disse!" i++)}
    }}
 </script>
