82c0e45854d5ce6a84e4582cebe56f6f41b7fb31
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-10 07:58:10 -0300

    Update script.js

--- Projeto - Gerador de senha/script.js
+++ Projeto - Gerador de senha/script.js
@@ -1,31 +1,24 @@
 
-let sliderElemento = windows.document.querySelector("#slider")
+ let sliderElemento = windows.document.querySelector("#slider")
+ let botãoElemento = windows.document.querySelector("#botão-senha")
+ let tamanhoSenha = windows.document.querySelector("#valor")
+ let senha = windows.document.querySelector("#senha")
+ let containerSenha = windows.document.querySelector("#container-senha")
+ let charset = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789!@#$%&*+?"
+ let novaSenha = "sizeSenha"
+  tamanhoSenha.innerHTML = sliderElemento.value
+   slider.oninput = function atualizaSlider(){
+    sizeSenha.innerHTML = this.value
+   }
 
-let botãoElemento = windows.document.querySelector("#botão-senha")
-
-let tamanhoSenha = windows.document.querySelector("#valor")
-
-let senha = windows.document.querySelector("#senha")
-
-let containerSenha = windows.document.querySelector("#container-senha")
-
-let charset = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789!@#$%&*+?"
-
-let novaSenha = "sizeSenha"
-
-tamanhoSenha.innerHTML = sliderElemento.value
-
-slider.oninput = function atualizaSlider(){
-	sizeSenha.innerHTML = this.value
-}
-
-function geraSenha(){
-	let pass = ""
-	for(let i = 0; n = charset.lenght; i < sliderElemento.value; ++i){
+  function geraSenha(){
+   let pass = ""
+    for(let i = 0; n = charset.lenght; i < sliderElemento.value; ++i){
 		pass += charset.charAt(math.floor(math.random() * n))
-}
+    }
+ 
+   alert("Senha gerada com sucesso, sua senha é: " + pass)	
 
-	alert("Senha gerada com sucesso, sua senha é: " + pass)	
-}
+  }
 
 
