4d443c345b078bc145292c733c484107616ff63f
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-29 14:56:56 -0300

    alguns erros
    mudei a estrutura dos ifs

--- Projeto - Calcula IMC.js/Index.js
+++ Projeto - Calcula IMC.js/Index.js
@@ -11,12 +11,12 @@
  var altura = parseInt(number(prompt("Massa! agora digite a sua altura por favor: ")))
   for(let i=0; i<1; ++i){
     if(CalculaIMC<18.5){(prompt("Voce mora na Africa? ")}
-     if("sim" || "não"){document.write("Sem piadinhas com a Africa ok? digite o seu real peso paliaso." i++)
+     if("sim" || "não"){document.write("Sem piadinhas com a Africa ok? digite o seu real peso paliaso.")
      } 
-      if(CalculaIMC<25){mostraUmaFrase("Parabens seu IMC esta na media! Voce esta parecendo um(a) deus(a) grego(a), não se preocupe, voce não vai voar com o vento nem ser confundido(a) com um pneu de posto de Gasolina, siga se cuidando assim!" i++)}
-       if(CalculaIMC<30){mostraUmaFrase("Voce esta com sobrepeso, esta parecendo o Majin Boo, precisa fazer mais exercicios e se alimentar melhor." i++)}
-        if(CalculaIMC>30){mostraUmaFrase("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou começar a praticar Sumo." i++)}
-   else{mostraUmaFrase("desculpe, mas não entendi pulufas do que voce disse!" i++)}
+      if(CalculaIMC<25){mostraUmaFrase("Parabens seu IMC esta na media! Voce esta parecendo um(a) deus(a) grego(a), não se preocupe, voce não vai voar com o vento nem ser confundido(a) com um pneu de posto de Gasolina, siga se cuidando assim!")}
+       if(CalculaIMC<30){mostraUmaFrase("Voce esta com sobrepeso, esta parecendo o Majin Boo, precisa fazer mais exercicios e se alimentar melhor.")}
+        if(CalculaIMC>30){mostraUmaFrase("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou começar a praticar Sumo.")}
+   else{mostraUmaFrase("desculpe, mas não entendi pulufas do que voce disse!")}
    }}
 </script>
 </body>
