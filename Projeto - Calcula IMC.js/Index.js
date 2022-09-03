ba116d37ae06a3e1fe0ec4e2986a9914677a7fe9
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-09-03 03:29:39 -0300

    Update Index.js
    conclui o projeto

--- Projeto - Calcula IMC.js/Index.js
+++ Projeto - Calcula IMC.js/Index.js
@@ -2,22 +2,22 @@
 <meta charset="UTF-8">
 
 <script>
-
+   
  function mostraResultado(frase){
- document.write(frase)
+  frase
  }
 
  function calculaIMC(peso, altura){ 
- return peso/(altura * altura)
+  return peso/(altura * altura)
  }
 
  var nomeDaPessoa = prompt("Por favor! informe o seu nome: ")
  var pesoDaPessoa = prompt("Digite Agora o seu peso: ")
  var alturaDaPessoa = prompt("Agora por ultimo digite a sua altura por favor: ")
  var imc = calculaIMC(pesoDaPessoa, alturaDaPessoa)
- mostraResultado(nomeDaPessoa + " o seu IMC é " + imc)
- 
-</script>
+ var resultado = alert(nomeDaPessoa + " o seu IMC é " + imc)
+
+ </script>
 
 
 
