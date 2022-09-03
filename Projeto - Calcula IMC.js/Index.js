4a9ef8d88fdba36d5c95f48a72a5fec85b4b5e4c
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-09-03 02:55:57 -0300

    consertando o projeto
    fiz um grande arrumancia no codigo que estava ruim

--- Projeto - Calcula IMC.js/Index.js
+++ Projeto - Calcula IMC.js/Index.js
@@ -1,28 +1,23 @@
 
-<!DOCTYPE html>
-<html>
-<head>
- <meta charset="utf-8">
- <meta name="viewport" content="width=devise-width, initial-scale=1.0">
- <meta name="keywords" content="Gerador de senha, generator password, cria senha, create password">
-</head>
-<body>
+<meta charset="UTF-8">
+
 <script>
- function mostraUmaFrase(frase){document.write(frase)}
- function CalculaIMC(peso, altura){peso/(altura*2)}
- var peso = parseInt(number(prompt("Ola! digite aqui o seu peso: ")))
- var altura = parseInt(number(prompt("Massa! agora digite a sua altura por favor: ")))
-  for(let i=0; i<1; ++i){
-    if(CalculaIMC<18.5){(prompt("Voce mora na Africa? ")}
-     if("sim" || "não"){document.write("Sem piadinhas com a Africa ok? digite o seu real peso paliaso.")
-     } 
-      if(CalculaIMC<25){mostraUmaFrase("Parabens seu IMC esta na media! Voce esta parecendo um(a) deus(a) grego(a), não se preocupe, voce não vai voar com o vento nem ser confundido(a) com um pneu de posto de Gasolina, siga se cuidando assim!")}
-       if(CalculaIMC<30){mostraUmaFrase("Voce esta com sobrepeso, esta parecendo o Majin Boo, precisa fazer mais exercicios e se alimentar melhor.")}
-        if(CalculaIMC>30){mostraUmaFrase("Voce provavelmente esta muito gordo, recomendo fazer mais exercicios ou começar a praticar Sumo.")}
-   else{mostraUmaFrase("desculpe, mas não entendi pulufas do que voce disse!")}
-   }}
+
+ function mostraImc(frase){
+  document.write(frase)
+ }
+
+ function calculaIMC(peso, altura){ 
+  return peso/(altura * altura)
+ }
+
+ var nomeDaPessoa = prompt("por favor! informe o seu nome: ")
+ var pesoDaPessoa = prompt("Ok " + nomeDaPessoa + "! " +"agora digite aqui o seu peso: ")
+ var alturaDaPessoa = prompt("Entendi " + nomeDaPessoa + "! " + "Agora digite a sua altura por favor: ")
+ var imc = calculaIMC(pesoDaPessoa, alturaDaPessoa)
+ document.alert(mostraImc(nomeDaPessoa + "!" + " o seu IMC é " + imc))
+ 
 </script>
-</body>
-</html>
+
 
 
