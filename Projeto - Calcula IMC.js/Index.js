f5b7a8f990c0a0f72413a0631cba3e6f629a3d58
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-09-18 16:52:13 -0300

    Update Index.js

--- Projeto - Calcula IMC.js/Index.js
+++ Projeto - Calcula IMC.js/Index.js
@@ -1,8 +1,8 @@
 <head>
-<meta charset="UTF-8">
+ <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="stylesheet" href="Reset.css"
- <head>
+ </head>
 <script>
 function mostraResultado(frase){
 frase
