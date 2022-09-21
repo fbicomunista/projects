e2aa2ad686716ec33256564daf3a21beae641a61
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-09-21 18:57:37 -0300

    continuando o projeto

--- Projeto - Gerador de senha/script.js
+++ Projeto - Gerador de senha/script.js
@@ -5,19 +5,27 @@
  let senha = windows.document.querySelector("#senha")
  let containerSenha = windows.document.querySelector("#container-senha")
  let charset = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789!@#$%&*+?"
- let novaSenha = "sizeSenha"
+ let novaSenha = ""
+ 
   tamanhoSenha.innerHTML = sliderElemento.value
-   slider.oninput = function atualizaSlider(){
+   slider.oninput = function (){
     sizeSenha.innerHTML = this.value
    }
 
   function geraSenha(){
    let pass = ""
-    for(let i = 0; n = charset.lenght; i < sliderElemento.value; ++i){
+    for(let i = 0, n = charset.lenght; i < sliderElemento.value; ++i){
 		pass += charset.charAt(math.floor(math.random() * n))
     }
- 
-   alert("Senha gerada com sucesso, sua senha é: " + pass)	
-   }
+	  
+	  containerPassword.classList.remove("hide")
+    password.innerHTML = pass;
 
+    novaSenha = pass;
+	  }
+
+function copiaSenha(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
 
