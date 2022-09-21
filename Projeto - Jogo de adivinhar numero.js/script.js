7e9ea17a4f6de082b99345a3b1f64daaec998fa9
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-09-21 19:43:30 -0300

    continuação do rojeto

---
+++ Projeto - Jogo de adivinhar numero.js/script.js
@@ -0,0 +1,13 @@
+ var numeroPensado = Math.round(Math.random() * 10) 
+ var tentativas = 1
+  while(tentativas <= 3){
+   var chute = parseInt(prompt("Digite seu chute: "))
+    if(chute == numeroPensado){
+     console.log("Você descobriu!, o número pensado era " + numeroPensado)
+    break
+  } else {
+ console.log("Você não descobriu antes das 3 tentativas!")
+        }
+tentativas++
+    }
+console.log("fim")
